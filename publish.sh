93a18e72336616d909f83da24c9f51f30e8dec60
francesco <info.francescobianco@gmail.com>
2019-05-29 15:10:17 +0200

    publish

--- publish.sh
+++ publish.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 set -e
 
-docker-compose down -v > /dev/null
-docker-compose pull vtiger
-docker-compose up -d
+while IFS=, read -r prefix name label version || [[ -n "${prefix}" ]]; do
+    mkdir -p build/modules
 
-git add . > /dev/null
-git commit -am "$*"
+    cd build
+
+    sed -e 's!__PREFIX__!'"${prefix}"'!' \
+        -e 's!__NAME__!'"${name}"'!' \
+        -e 's!__LABEL__!'"${label}"'!' \
+        -e 's!__VERSION__!'"${version}"'!' \
+        ../manifest.xml > manifest.xml
+
+    cp -R ../languages/${prefix}/* modules
+
+    zip -r ../packages/LanguagePack-${name}-${prefix}.zip manifest.xml modules
+
+    cd .. && rm -fr build
+
+done < packages.txt
+
+git add .
+git commit -am "publish"
 git push
-
-echo ""
-docker-compose ps
